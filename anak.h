8ea6ccee5cad05b313df5b314282e62a5057489b
devano2068 <devano27921@gmail.com>
2025-05-26 20:37:06 +0700

    Menyesuaikan indentasi

--- anak.h
+++ anak.h
@@ -5,12 +5,10 @@
 class anak {
 public:
     string nama;
-    // konstruktor
     anak(string pNama) :nama(pNama)
     {
         cout << "Anak \"" << nama << "\" ada\n";
     }
-    // destruktor
     ~anak()
     {
         cout << "Anak \"" << nama << "\" tidak ada\n";
